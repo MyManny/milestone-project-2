9f01b17dc26e00fd1f4ad7d14129bb55861f5969
Sethgood7 <sethgooodman657@gmail.com>
2023-10-25 14:58:23 -0700

    trying to push to my own branch

--- client/src/App.js
+++ client/src/App.js
@@ -5,17 +5,17 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-    const [message, setMessage] = useState("Loading...");
-    const [secret, setSecret] = useState("Loading...");
+  const [message, setMessage] = useState("Loading...");
+  const [secret, setSecret] = useState("Loading...");
 
-    const API_URL = process.env.REACT_APP_API_URL || "http://localhost:5000";
+  const API_URL = process.env.REACT_APP_API_URL || "http://localhost:5000";
 
-    useEffect(() => {
-        axios.get(`${API_URL}/`).then((resp) => {
-            setMessage(resp.data.message);
-            setSecret(resp.data.secret);
-        });
-    }, []);
+  useEffect(() => {
+    axios.get(`${API_URL}/`).then((resp) => {
+      setMessage(resp.data.message);
+      setSecret(resp.data.secret);
+    });
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
@@ -32,7 +32,7 @@ function App() {
           <p>Brett is pushing something</p>
           Learn React
           <p> Ben McConnaughy </p>
-          <p>this is seth</p>
+          <p>this is seth, writing more to try and push to my own branch</p>
           <p>Esteban Sepulveda</p>
         </a>
         <p>DavidVidal</p>
