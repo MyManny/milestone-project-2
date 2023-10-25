acb52b41479191d2747fb3b655e4eb219d83b8a8
Benjamin McConnaughy <bhmcconn@gmail.com>
2023-10-24 21:31:09 -0700

    installed axios implmented env to navigate deployment

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+    const [message, setMessage] = useState("Loading...");
+    const [secret, setSecret] = useState("Loading...");
+
+    const API_URL = process.env.REACT_APP_API_URL || "http://localhost:5000";
+
+    useEffect(() => {
+        axios.get(`${API_URL}/`).then((resp) => {
+            setMessage(resp.data.message);
+            setSecret(resp.data.secret);
+        });
+    }, []);
   return (
     <div className="App">
       <header className="App-header">
