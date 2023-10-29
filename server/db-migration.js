f1360ab981d8bffba5ec954a24e1075291db944e
Benjamin McConnaughy <bhmcconn@gmail.com>
2023-10-28 19:40:48 -0700

    made table

--- server/db-migration.js
+++ server/db-migration.js
@@ -1,22 +1,22 @@
 const client = require("./db-client");
 
-const createUserTableQuery = `
-CREATE TABLE IF NOT EXISTS "User" (
-    id SERIAL PRIMARY KEY,
-    username VARCHAR(255) UNIQUE NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    token VARCHAR(255) DEFAULT NULL
-);
-`;
+// const createUserTableQuery = `
+// CREATE TABLE IF NOT EXISTS "User" (
+//     id SERIAL PRIMARY KEY,
+//     username VARCHAR(255) UNIQUE NOT NULL,
+//     password VARCHAR(255) NOT NULL,
+//     token VARCHAR(255) DEFAULT NULL
+// );
+// `;
 
-const createTodoTableQuery = `
-CREATE TABLE IF NOT EXISTS "Todo" (
-    id SERIAL PRIMARY KEY,
-    user_id INTEGER REFERENCES "User"(id),
-    name VARCHAR(255) NOT NULL,
-    completed BOOLEAN DEFAULT false
-);`;
-const createTravelTable = `
+// const createTodoTableQuery = `
+// CREATE TABLE IF NOT EXISTS "Todo" (
+//     id SERIAL PRIMARY KEY,
+//     user_id INTEGER REFERENCES "User"(id),
+//     name VARCHAR(255) NOT NULL,
+//     completed BOOLEAN DEFAULT false
+// );`;
+const createTravelTableQuery = `
 CREATE TABLE IF NOT EXISTS "Travel" (
     event_id SERIAL PRIMARY KEY,       
     event_title VARCHAR(255) NOT NULL, 
@@ -34,12 +34,12 @@ CREATE TABLE IF NOT EXISTS "Travel" (
 async function createTables() {
     try {
         // Create User table
-        await client.query(createUserTableQuery);
-        console.log('"User" table created successfully.');
+        // await client.query(createUserTableQuery);
+        // console.log('"User" table created successfully.');
 
         // Create Todo table
-        await client.query(createTodoTableQuery);
-        console.log('"Todo" table created successfully.');
+        await client.query(createTravelTableQuery);
+        console.log('"Travel" table created successfully.');
     } catch (err) {
         console.error("Error creating tables:", err);
     } finally {
