1ffe172281c9dcea2e4bb45170b034dcf666b2e7
Sethgood7 <sethgooodman657@gmail.com>
2023-10-30 15:06:40 -0700

    created todo list functionality

--- client/src/App.js
+++ client/src/App.js
@@ -15,29 +15,13 @@ function App() {
       setSecret(resp.data.secret);
     });
   }, [API_URL]);
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <p> Brett is pushing something </p>
-          Learn React
-          <p> Ben McConnaughy is trying to make the branch work </p>
-          <p> this is seth </p>
-          <p> Esteban Sepulveda </p>
-        </a>
-        <p> DavidVidal </p>
-      </header>
+    <div>
+      <HomePage />
     </div>
   )
 }
 
 export default App;
+
---
+++ client/src/Components/TodoForm.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from 'react'
+
+function TodoForm(props) {
+    const [input, setInput] = useState('');
+
+    const handleChange = e => {
+        setInput(e.target.value);
+    };
+
+    const handleSubmit = e => {
+        e.preventDefault();
+
+        props.onSubmit({
+            id: Math.floor(Math.random() * 50000),
+            text: input
+        });
+
+        setInput('')
+    };
+
+    return (
+        <div>
+            <form className='todo-form' onSubmit={handleSubmit}>
+                <input 
+                type="text" 
+                placeholder='Add a todo' 
+                value={input} 
+                name='text' 
+                className='todo-input'
+                onChange={handleChange}
+            />
+                <button className='todo-button'>Add Todo</button>
+            </form>
+        </div>
+    )
+}
+
+export default TodoForm
