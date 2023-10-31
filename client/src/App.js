cb21f15eb8fca84567c2ad572c3b16cf9829593c
Benjamin McConnaughy <bhmcconn@gmail.com>
2023-10-31 15:49:40 -0700

    added router

--- client/src/App.js
+++ client/src/App.js
@@ -12,7 +12,7 @@ function App() {
   const [message, setMessage] = useState("Loading...");
   const [secret, setSecret] = useState("Loading...");
 
-  const API_URL = process.env.REACT_APP_API_URL || "http://localhost:5000";
+  const API_URL = process.env.REACT_APP_API_URL || "http://localhost:5002";
 
 
   useEffect(() => {
