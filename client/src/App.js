5b0229359117cb02981e2b990686a8a9b58e11b5
Benjamin McConnaughy <bhmcconn@gmail.com>
2023-10-31 15:27:29 -0700

    please work

--- client/src/App.js
+++ client/src/App.js
@@ -12,7 +12,7 @@ function App() {
   const [message, setMessage] = useState("Loading...");
   const [secret, setSecret] = useState("Loading...");
 
-  const API_URL = process.env.REACT_APP_API_URL || "http://localhost:3000";
+  const API_URL = process.env.REACT_APP_API_URL || "http://localhost:5000";
 
 
   useEffect(() => {
