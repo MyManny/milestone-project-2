894c9f6404784cae8f71ca986254b1cd7678c02c
Sethgood7 <sethgooodman657@gmail.com>
2023-11-02 23:06:09 -0700

    pulled from local main

--- client/src/Components/HomePage.jsx
+++ client/src/Components/HomePage.jsx
@@ -62,7 +62,7 @@ export default function HomePage() {
          className={isDarkMode ? "dark--mode--fill" : "light--mode--fill"} />
           <title>DarkMode Toggle</title>
         </svg>
-        {todos && <TodoList className={isDarkMode ? "dark--mode--fill" : "light--mode--fill"}/>}
+        {todos && <TodoList/>}
         <svg
           xmlns="http://www.w3.org/2000/svg"
           className={`plus--btn ${isX ? "x" : ""}`}
